0eff47960753d486a5d44f5bd79815cbb7b314ff
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-20 19:21:53 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "exrecord", description: "A tiny library to facilitate record upgrades in Elixir",
+                 version: :head, keywords: ["record","upgrade","release","state"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/exrecord"]])
+    
